7b1246783ce2e179aaa6129b3291605821845e25
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-11-19 20:41:02 +0700

    feat: add unit test for successful user registration in auth service

--- apps/auth/service_test.go
+++ apps/auth/service_test.go
@@ -1,1 +1,39 @@
 package auth
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/ardwiinoo/online-shop/external/database"
+	"github.com/ardwiinoo/online-shop/internal/config"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+var svc service
+
+func init() {
+	filename := "../../cmd/api/config.yaml"
+	err := config.LoadConfig(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := database.ConnectPostgres(config.Cfg.DB)
+	if err != nil {
+		panic(err)
+	}
+
+	repo := newRepository(db)
+	svc = newService(repo)
+}
+
+func TestRegister_Success(t *testing.T) {
+	req := RegisterRequestPayload{
+		Email:    fmt.Sprintf("%v@gmail.id", uuid.NewString()),
+		Password: "secret",
+	}
+	err := svc.register(context.Background(), req)
+	require.Nil(t, err)
+}
