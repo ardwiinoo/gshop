a35b03f80fca8306fd3a9d106826ecc1f5393e70
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-12-08 11:25:03 +0700

    feat: add stock management and update methods to Product entity

--- apps/transaction/entity_product.go
+++ apps/transaction/entity_product.go
@@ -1,8 +1,25 @@
 package transaction
 
+import "github.com/ardwiinoo/online-shop/infra/response"
+
 type Product struct {
 	Id    int    `db:"id" json:"id"`
 	SKU   string `db:"sku" json:"sku"`
 	Name  string `db:"name" json:"name"`
+	Stock int    `db:"stock" json:"stock"`
 	Price int    `db:"price" json:"price"`
+}
+
+func (p Product) IsExists() bool {
+	return p.Id != 0
+}
+
+func (p *Product) UpdateStockProduct(amount uint8) (err error) {
+	if p.Stock < int(amount) {
+		return response.ErrAmountGreaterThanStock
+	}
+
+	p.Stock = p.Stock - int(amount)
+
+	return
 }
